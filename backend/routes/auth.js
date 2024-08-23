31484b1812824175813cb38c2f60d257f0315676
unknown <venkataramanakoppisetti25@gmail.com>
2024-08-23 21:36:46 +0530

    Backend Setup - Added bcryptjs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require('../database');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const secretKey = "VENKATARAMANA_SECRET_TOKEN"; 
 
